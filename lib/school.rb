4872465d0f817f2aa7c9b017806fd0b711e952a6
Huy Do <huyddo40@gmail.com>
2019-06-17 02:53:14 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,25 @@
-# code here!
+class School
+  attr_accessor :name, :roster
+
+  def initialize(name, roster)
+    @name = name
+    @hash = {}
+  end
+
+  def length
+    @hash.length = 0
+  end
+
+  def roster
+    length
+  end
+
+  def add_student(name, grade)
+    @roster[grade] = []
+    @roster[grade] << name
+  end
+
+  def grade(grade)
+    roster[grade]
+  end
+end
