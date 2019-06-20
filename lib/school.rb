d8cc64893cd712ebde583bbc02b5b34a4819ca10
Khris Punzalan <khrispunzalan@gmail.com>
2019-06-20 12:09:15 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,23 +1,38 @@
 # code here!
 class School
-  attr_accessor :name, :roster, :student, :grade
+  attr_accessor :name, :roster
 
     def intialize(name)
       @name = name
       @roster = {}
     end
-    
+
     def add_student(name, grade)
       @roster[grade] ||= []
       @roster[grade] << student
     end
-  
+
+# ||= if condition is true, create this array
+
+# from SG:
+# def add_student(name, grade)
+#   if roster.has_keys?(grade) or roster[grade]
+#     roster[grade] << name
+#   else
+#     roster[grade] = [name]
+#   end
+# end
+
     def grade(grade)
      @grade
     end
-  
+
     def sort
-      strings = @roster
+      sorted={}
+      roster.each do |grade, name|
+        sorted[grade] = name.sort
+      end
+      sorted
     end
 
 end
