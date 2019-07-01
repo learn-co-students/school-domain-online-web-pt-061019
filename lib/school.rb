5b90a85b6a152796271722adba6c42503cfe45e3
Hillary Spitzer <hspitzer2@gmail.com>
2019-07-01 21:57:37 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,18 @@
-# code here!
+class School
+  attr_reader :name, :roster
+
+def initialize(name, grade)
+  @name = name
+  @roster = {}
+end
+
+def add_student(name, grade)
+  if roster(grade)<< name
+ else
+   roster[grade] = [name]
+ end
+end
+
+
+
+end# code here!
