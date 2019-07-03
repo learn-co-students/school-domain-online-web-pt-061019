6dd89d83fae7bd2b8a43a39342e8f1b944407cef
Christian Polanco <chrispolanco@sbcglobal.net>
2019-07-03 02:46:25 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
-# code here!
+class School
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def name=(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+  def roster=(roster)
+    @roster = roster
+  end
+
+  def roster
+    @roster
+  end
+
+  def add_student(name, number_grade)
+    roster[number_grade] = [name]
+  end 
+end
