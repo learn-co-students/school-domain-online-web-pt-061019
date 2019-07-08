2fd2aea503a7268a79d05d5782df9aa834e83d8d
Dex Fitch <fitch.dexter@gmail.com>
2019-07-08 14:58:27 -0700

    learnin

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
-# code here!
+class School
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def roster
+    @roster
+  end
+
+  def add_student(name, grade)
+    if @roster.key?(grade)
+      @roster[grade] << name
+    else
+      @roster[grade] = [name]
+    end
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster = @roster.each { |grade, name| @roster[grade].sort! }
+    @roster.sort.to_h
+  end
+end
