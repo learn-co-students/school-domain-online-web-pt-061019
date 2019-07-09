09ec0f56111345566e7374ee34502242d7f7568d
Huy Do <huyddo40@gmail.com>
2019-07-09 01:02:43 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -21,6 +21,7 @@ class School
   end
 
   def sort
+
     hash ={}
     @roster.each do |key, val|
       hash[key] = val.sort #sort array name and assign it to new hash
@@ -28,5 +29,17 @@ class School
     hash.sort_by{|k,v| k} #sort by key and return array
     hash.to_h #change array to hash
       # binding.pry
+
+    # hash = @roster.sort_by { |k, v| [k,v] }
+    # hash.to_h
+    # hash.each do |key, val|
+    #   val.sort do |a,b|
+    #     a <=> b
+    #   end
+    #   # binding.pry
+    # end
+    #
+    # # binding.pry
+
   end
 end
