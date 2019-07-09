de0c85818659c593e50ae96a151edadf4b3be72d
Hillary Spitzer <hspitzer2@gmail.com>
2019-07-09 23:17:35 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,18 +1,19 @@
 class School
   attr_reader :name, :roster
 
-def initialize(name, grade)
-  @name = name
-  @roster = {}
+  def initialize(name)
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    @roster[grade] ||= []
+    @roster[grade] << name
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+  def sort
+    @roster.each {|key,value| value.sort!}
+   end
 end
-
-def add_student(name, grade)
-  if roster(grade)<< name
- else
-   roster[grade] = [name]
- end
-end
-
-
-
-end# code here!
