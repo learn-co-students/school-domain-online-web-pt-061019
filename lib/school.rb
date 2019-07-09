a1f1d18f495c6d1e502a53c47b6d94904057fa69
Lantz Warrick <betalantz@gmail.com>
2019-07-09 21:23:29 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -2,26 +2,47 @@ class School
 
   attr_accessor :roster, :name
 
-  def initialize(name)
-    @name = name
+  def initialize(school_name)
+    @name = school_name
     @roster = {}
   end
 
   def add_student(student_name, grade)
-    roster[grade] ||= []
-    roster[grade] << student_name
+    #using or-equal operator
+    @roster[grade] ||= []
+    @roster[grade] << student_name
+
+    #using if-conditional
+    #if @roster[grade] == nil
+    #  @roster[grade] = [student_name]
+    #else
+    #  @roster[grade] << student_name
+    #end
+
+    #using ternary
+    # @roster[grade] ? @roster[grade] << student_name : @roster[grade] = [student_name]
   end
 
   def grade(grade)
-    roster[grade]
+    @roster[grade]
+
+    # roster[grade] will also work b/c roster calls the roster getter created by
+    # attr_accessor :roster which returns @roster
+
   end
 
   def sort
+    #non-destructive method
     sorted = {}
-    roster.map {|grade, students|
+    @roster.map {|grade, students|
       sorted[grade] = students.sort
     }
     sorted
+
+    #destructive method
+    #roster.each do |grade, names|
+    #  names.sort!
+    #end
   end
 
 end
