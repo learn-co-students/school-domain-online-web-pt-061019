6592336d7dcc3776d23d42c5e48e1bd454447c91
Aida Abdikulova <abdikulova@gmail.com>
2019-07-11 02:30:53 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
-# code here!
+class School
+
+  attr_accessor :roster, :name
+
+  def initialize(name)
+    @roster = {}
+  end
+
+  def add_student(student, grade)
+    if @roster[grade] == nil
+      @roster[grade] = [student]
+    else
+      @roster[grade] << student
+    end
+  end
+
+  def grade(grade)
+    return @roster[grade]
+  end
+
+  def sort()
+    @roster.map do |key, value|
+      value.sort!
+  end
+  return @roster
+end
+
+end
